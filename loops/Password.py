e5f1f45e34a05d2051f18654cdf4b9417458b043
Jatin Rathi <65804761+JATIN-RATHI@users.noreply.github.com>
2020-10-01 00:44:52 +0530

    Update Password.py
    Use f string to display in a good way.

--- loops/Password.py
+++ loops/Password.py
@@ -4,10 +4,10 @@ passWord = ""
 while passWord != "redhat":
     passWord = input("Please enter the password: ")
     if passWord == "redhat":
-        print("Correct password")
+        print("Correct password!")
     elif passWord == "admin@123":
         print("It was previously used password")
     elif passWord == "Redhat@123":
-        print("This is your recent changed password")
+        print(f"{passWord} is your recent changed password")
     else:
-        print("Incorrect Password- try again")
+        print("Incorrect Password- try again!")
